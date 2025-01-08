a793dc4c6a8049d72c98ed1a7680b995a7eac85f
Saul Echeverri <shecheve@bancolombia.com.co>
2025-01-08 10:34:16 -0500

    First commit

---
+++ src/main/java/org/educatiom/lesson01_OOP_EncapsulaciónYPolimorfismo/OOPConceptosBasicosExample.java
@@ -0,0 +1,32 @@
+package org.educatiom.lesson01_OOP_EncapsulaciónYPolimorfismo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class OOPConceptosBasicosExample {
+
+    public static void main(String[] args) {
+
+        System.out.println("--------------------- Pilares de la POO ---------------------------");
+        //Abstracción
+        /*Simplificar la complejidad mostrando solo los detalles esenciales de un objeto y ocultando los innecesarios.
+        Ejemplo: Una clase Coche puede tener atributos como marca y modelo, pero no necesita exponer cómo funciona
+        internamente el motor.*/
+
+        //Encapsulamiento
+        /*Proteger los datos de un objeto restringiendo el acceso directo y permitiendo su manipulación solo a través de
+        métodos específicos.
+        Ejemplo: Usar métodos get y set para acceder a atributos privados.*/
+
+        //Herencia
+        /*Permitir que una clase (subclase) herede propiedades y comportamientos de otra clase (superclase), promoviendo
+        la reutilización del código.
+        Ejemplo: Una clase Vehículo puede ser la base de Coche y Moto.*/
+
+        //Polimorfismo
+        /*Permitir que los objetos de diferentes clases relacionadas se comporten de manera diferente usando una interfaz
+         común.
+         Ejemplo: Un method mover() puede tener implementaciones distintas en las clases Coche y Bicicleta.*/
+
+    }
+}
